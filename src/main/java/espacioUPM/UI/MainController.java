7d66a534ca2dfac720e0cd142a905eb5a81e6cec
Eduardo Alonso <edu@error404software.com>
2019-05-12 19:40:01 +0200

    Mala mía 🤷‍♂️

--- src/main/java/espacioUPM/UI/MainController.java
+++ src/main/java/espacioUPM/UI/MainController.java
@@ -20,7 +20,7 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 public class MainController {
-    private static Stage mStage;
+    private Stage mStage;
     @FXML TextField txtAlias;
     @FXML PasswordField txtPass;
 
