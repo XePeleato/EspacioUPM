eb3fb4e705d7d8c4a8a5e65f8a0eeb08fe8c6c0e
Jonny <jonnyzzz10@gmail.com>
2019-05-20 19:39:13 +0200

    Esqueleto de la pruebas

--- src/test/java/espacioUPM/DBTest.java
+++ src/test/java/espacioUPM/DBTest.java
@@ -2,10 +2,16 @@ package espacioUPM;
 
 import espacioUPM.Database.DB_Main;
 import espacioUPM.Database.IDB_Usuario;
+import espacioUPM.Publicaciones.Comentario;
+import espacioUPM.Publicaciones.Publicacion;
+import espacioUPM.Publicaciones.Puntuacion;
 import org.junit.*;
+
+import java.util.ArrayList;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertNull;
 
 public class DBTest {
@@ -50,4 +56,150 @@ public class DBTest {
 
         assertNull(us2);
     }
-}
+
+    @Test
+    public void TestgetUsuario(String alias){
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestBuscarUsuario(String alias){
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestSetUsuario(String alias, String correo, String pass) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetNewID() {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestSetPublicacion(Publicacion publi) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetPublicacion(int id) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetPublicaciones(Usuario usuario) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetComentarios(int publication_id) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetLikes(int publication_id) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetDislikes(int publication_id) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestBorrarPublicacion(int publi) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetSeguidos(Usuario usuario) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestgetSeguidores(Usuario usuario) {
+        fail("No esta implementado todavia");
+    }
+    @Test
+    public void TestCambiarAlias(Usuario usuario, String aliasNuevo) {
+        fail("No esta implementado todavia");
+    }
+    @Test
+    public void TestBorrarUsuario(Usuario usuario) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestBorrarMiembroComunidad(String id, String alias) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestInsertarMiembroComunidad(String id, String alias) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestAceptarMiembroComunidad(String id, String alias) {
+        fail("No esta implementado todavia");
+    }
+    @Test
+    public void TestSeguir(String seguidor, String seguido) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestDejarDeSeguir(String seguidor, String seguido) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestEstaSiguiendo(String aliasSeguidor, String aliasSeguido) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestPuntuar(Usuario usuario, int publi, Puntuacion puntuacion) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetPuntuacion(Usuario usuario, int publi) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestHacerAdminComunidad(String id, String alias) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetMiembros(Comunidad comunidad) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestGetTimeline(Comunidad comunidad) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestBuscarComunidad(String id) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestComentar(Publicacion publi, Usuario usuario, String contenido) {
+        fail("No esta implementado todavia");
+    }
+
+    @Test
+    public void TestComprobarPasswd(String alias, String passwd) {
+        fail("No esta implementado todavia");
+    }
+
+
+
+
+    }
